290a28c344eada19152469df50d2850ba295b224
Maurice Velte <maurice.velte@cronn.de>
2018-03-13 17:45:10 +0100

    set fake test working dir through dependency injection instead of mockito

--- src/test/java/de/cronn/diff/MainNoOutputTest.java
+++ src/test/java/de/cronn/diff/MainNoOutputTest.java
@@ -1,25 +1,28 @@
 package de.cronn.diff;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mockito;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 
-import de.cronn.diff.util.FileHelper;
-
-@RunWith(PowerMockRunner.class)
-@PrepareForTest({FileHelper.class})
 public class MainNoOutputTest extends MainTestBase {
 
+	private String workingDirBkp;
+
+	@Before
+	public void backupMainWorkingDir() {
+		workingDirBkp = Main.workingDir;
+	}
+	
+	@After 
+	public void resetMainWorkingDir() {
+		Main.workingDir = workingDirBkp;
+	}
+	
 	@Test
-	public void testMainJAVADiffDirsToHtml_noOutputProvided() throws Exception {
-		PowerMockito.mockStatic(FileHelper.class);
-		Mockito.when(FileHelper.getWorkingDir()).thenReturn(TestBase.TEST_DATA_OUTPUT_DIR);
-		
+	public void testMainJAVADiffDirsToHtml_noOutputProvided() throws Exception {		
 		exit.expectSystemExitWithStatus(Main.EXIT_CODE_OK);
 		exit.checkAssertionAfterwards(new SystemMessageAssertion());
+		Main.workingDir = TestBase.TEST_DATA_OUTPUT_DIR;
 		Main.main(new String[] { INPUT_DIR_1, INPUT_DIR_1 });
 	}
 	
